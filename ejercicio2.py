f97729073bc478808edf7e035c0452c03ec18847
Jojo3235 <joralmand@gmail.com>
2023-05-25 09:38:08 +0200

    mas tests

---
+++ ejercicio2.py
@@ -0,0 +1,54 @@
+class Pokemon:
+    def __init__(self, nombre, tipo, ps, ataque, defensa, ataque_especial, defensa_especial, velocidad):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.ps = ps
+        self.ataque = ataque
+        self.defensa = defensa
+        self.ataque_especial = ataque_especial
+        self.defensa_especial = defensa_especial
+        self.velocidad = velocidad
+        print(f"Se ha creado el Pokemon {self.nombre} de tipo {self.tipo} con éxito.")
+
+    def clasificacion(self):
+        if self.tipo == "Agua":
+            return "Tipo agua"
+        elif self.tipo == "Fuego":
+            return "Tipo fuego"
+        elif self.tipo == "Planta":
+            return "Tipo planta"
+        elif self.tipo == "Electrico":
+            return "Tipo electrico"
+        elif self.tipo == "Normal":
+            return "Tipo normal"
+        elif self.tipo == "Psiquico":
+            return "Tipo psiquico"
+        elif self.tipo == "Fantasma":
+            return "Tipo fantasma"
+        elif self.tipo == "Roca":
+            return "Tipo roca"
+        elif self.tipo == "Volador":
+            return "Tipo volador"
+        elif self.tipo == "Bicho":
+            return "Tipo bicho"
+        elif self.tipo == "Tierra":
+            return "Tipo tierra"
+        elif self.tipo == "Hielo":
+            return "Tipo hielo"
+        elif self.tipo == "Lucha":
+            return "Tipo lucha"
+        elif self.tipo == "Veneno":
+            return "Tipo veneno"
+        elif self.tipo == "Acero":
+            return "Tipo acero"
+        elif self.tipo == "Dragon":
+            return "Tipo dragon"
+        elif self.tipo == "Siniestro":
+            return "Tipo siniestro"
+        elif self.tipo == "Hada":
+            return "Tipo hada"
+        else:
+            return "Tipo de Pokemon no reconocido."
+        
+    def __str__(self):
+        return f"Pokemon {self.nombre} de tipo {self.tipo} con {self.ps} puntos de salud, {self.ataque} puntos de ataque, {self.defensa} puntos de defensa, {self.ataque_especial} puntos de ataque especial, {self.defensa_especial} puntos de defensa especial y {self.velocidad} puntos de velocidad."
---
+++ test_ejercicio2.py
@@ -0,0 +1,19 @@
+from ejercicio2 import Pokemon
+import unittest
+
+class TestPokemon(unittest.TestCase):
+    def test_str(self):
+        poke1 = Pokemon('Charmander', 'Fuego', 39, 52, 43, 60, 50, 65)
+        self.assertEqual(str(poke1), "Pokemon Charmander de tipo Fuego con 39 puntos de salud, 52 puntos de ataque, 43 puntos de defensa, 60 puntos de ataque especial, 50 puntos de defensa especial y 65 puntos de velocidad.")
+
+        poke2 = Pokemon('Squirtle', 'Agua', 44, 48, 65, 50, 64, 43)
+        self.assertEqual(str(poke2), "Pokemon Squirtle de tipo Agua con 44 puntos de salud, 48 puntos de ataque, 65 puntos de defensa, 50 puntos de ataque especial, 64 puntos de defensa especial y 43 puntos de velocidad.")
+
+        poke3 = Pokemon('Bulbasaur', 'Planta', 45, 49, 49, 65, 65, 45)
+        self.assertEqual(str(poke3), "Pokemon Bulbasaur de tipo Planta con 45 puntos de salud, 49 puntos de ataque, 49 puntos de defensa, 65 puntos de ataque especial, 65 puntos de defensa especial y 45 puntos de velocidad.")
+
+        poke4 = Pokemon('Pikachu', 'Electrico', 35, 55, 40, 50, 50, 90)
+        self.assertEqual(str(poke4), "Pokemon Pikachu de tipo Electrico con 35 puntos de salud, 55 puntos de ataque, 40 puntos de defensa, 50 puntos de ataque especial, 50 puntos de defensa especial y 90 puntos de velocidad.")
+
+        poke5 = Pokemon('Eevee', 'Normal', 55, 55, 50, 45, 65, 55)
+        self.assertEqual(str(poke5), "Pokemon Eevee de tipo Normal con 55 puntos de salud, 55 puntos de ataque, 50 puntos de defensa, 45 puntos de ataque especial, 65 puntos de defensa especial y 55 puntos de velocidad.")
