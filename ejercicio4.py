a1f38fd6991fbb3d1bc55b7d40180150d495a36f
Jojo3235 <joralmand@gmail.com>
2023-05-25 09:05:43 +0200

    abra la puerta

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,45 +1,93 @@
-def contar_teletransportes_validos(n, k):
-    # Definir las rutas disponibles
-    rutas = [0, 1, 2, 3, 4, 6, 7, 8, 9]
+class Nodo:
+    def __init__(self, x, y, padre=None):
+        self.x = x
+        self.y = y
+        self.padre = padre
 
-    # Inicializar la matriz dp
-    dp = [[0 for _ in range(10)] for _ in range(k+1)]
-    for i in rutas:
-        dp[0][i] = 1
+class Pila:
+    def __init__(self):
+        self.items = []
 
-    # Calcular los valores de dp utilizando programación dinámica
-    for j in range(1, k+1):
-        for i in rutas:
-            if i == 0:
-                dp[j][i] = dp[j-1][6] + dp[j-1][8]
-            elif i == 1:
-                dp[j][i] = dp[j-1][7] + dp[j-1][9]
-            elif i == 2:
-                dp[j][i] = dp[j-1][4] + dp[j-1][8]
-            elif i == 3:
-                dp[j][i] = dp[j-1][0] + dp[j-1][4] + dp[j-1][9]
-            elif i == 4:
-                dp[j][i] = dp[j-1][2] + dp[j-1][3] + dp[j-1][9]
-            elif i == 6:
-                dp[j][i] = dp[j-1][0] + dp[j-1][1] + dp[j-1][7]
-            elif i == 7:
-                dp[j][i] = dp[j-1][2] + dp[j-1][6]
-            elif i == 8:
-                dp[j][i] = dp[j-1][0] + dp[j-1][2]
-            elif i == 9:
-                dp[j][i] = dp[j-1][1] + dp[j-1][3]
+    def esta_vacia(self):
+        return len(self.items) == 0
 
-    # Calcular la suma de todos los valores de dp[k][i]
-    total = sum(dp[k][i] for i in rutas)
+    def apilar(self, item):
+        self.items.append(item)
 
-    return total
+    def desapilar(self):
+        return self.items.pop()
+
+class Cola:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def encolar(self, item):
+        self.items.append(item)
+
+    def desencolar(self):
+        return self.items.pop(0)
+
+class ListaEnlazada:
+    def __init__(self):
+        self.cabeza = None
+
+    def esta_vacia(self):
+        return self.cabeza is None
+
+    def agregar(self, x, y, padre=None):
+        nodo = Nodo(x, y, padre)
+        if self.esta_vacia():
+            self.cabeza = nodo
+        else:
+            actual = self.cabeza
+            while actual.padre is not None:
+                actual = actual.padre
+            actual.padre = nodo
+
+    def obtener_camino(self):
+        camino = []
+        actual = self.cabeza
+        while actual is not None:
+            camino.append((actual.x, actual.y))
+            actual = actual.padre
+        camino.reverse()
+        return camino
+
+def resolver_abra(x_inicial, y_inicial, x_final, y_final):
+    movimientos = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+    visitados = set()
+    cola = Cola()
+    cola.encolar(Nodo(x_inicial, y_inicial))
+    while not cola.esta_vacia():
+        actual = cola.desencolar()
+        if actual.x == x_final and actual.y == y_final:
+            camino = ListaEnlazada()
+            while actual is not None:
+                camino.agregar(actual.x, actual.y, actual.padre)
+                actual = actual.padre
+            return camino.obtener_camino()
+        for dx, dy in movimientos:
+            x_nuevo = actual.x + dx
+            y_nuevo = actual.y + dy
+            if 1 <= x_nuevo <= 4 and 1 <= y_nuevo <= 3 and (x_nuevo, y_nuevo) not in visitados:
+                visitados.add((x_nuevo, y_nuevo))
+                cola.encolar(Nodo(x_nuevo, y_nuevo, actual))
+    return None
 
 def main():
-    casos = [1, 2, 3, 5, 8, 10, 15, 18, 21, 23, 32]
-    for n in casos:
-        for k in range(1, 4):
-            solucion = contar_teletransportes_validos(n, k)
-            print(f'Para n={n} y k={k}, la cantidad de posibles teletransportes válidos es {solucion}.')
+    # Ejemplo de uso
+    x_inicial, y_inicial = 1, 1
+    x_final, y_final = 4, 3
+    camino = resolver_abra(x_inicial, y_inicial, x_final, y_final)
+    if camino is not None:
+        print(f'Se encontró un camino de longitud {len(camino)}:')
+        for x, y in camino:
+            print(f'({x}, {y})')
+    else:
+        print('No se encontró un camino válido.')
 
 if __name__ == '__main__':
     main()
